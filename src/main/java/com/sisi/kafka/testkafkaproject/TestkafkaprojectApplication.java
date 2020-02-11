08e0cd953f8f2cfbdd960b8b22fff74c4a5044fe
Sisi <dqgsh1055@gmail.com>
2020-02-11 19:21:17 +0900

    Initial commit

---
+++ src/main/java/com/sisi/kafka/testkafkaproject/TestkafkaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sisi.kafka.testkafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestkafkaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestkafkaprojectApplication.class, args);
+    }
+
+}
